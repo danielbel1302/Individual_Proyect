519475e2b421547e9d5bf716cf60d7591e970986
Daniel Beltran <danielbel1302@gmail.com>
2022-07-16 03:18:17 -0300

    second commit

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,54 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define("country", {
+    id: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    flagImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.INTEGER,
+    },
+    poblation: {
+      type: DataTypes.STRING,
+    },
+  });
+  sequelize.define("touristActivity", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      min: 1,
+      max: 5,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+    },
+    season: {
+      type: DataTypes.ENUM("Verano", "Otoño", "Invierno", "Primavera"),
     },
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require("axios");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get("/countries", async(req, res) => {
+    const countries = await axios.get('https://restcountries.com/v3/all');
+    console.log(countries);
+    res.send(countries[0]);
+});
 
 module.exports = router;
