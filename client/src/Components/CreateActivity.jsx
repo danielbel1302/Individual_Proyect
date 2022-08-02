36104518189d62cf331624c29b7e0b37952415eb
Daniel Beltran <danielbel1302@gmail.com>
2022-08-02 00:24:16 -0300

    tenth commit

--- client/src/Components/CreateActivity.jsx
+++ client/src/Components/CreateActivity.jsx
@@ -5,12 +5,24 @@ import style from "./CreateActivity.module.css";
 
 export default function CreateActivity() {
   const countries = useSelector((state) => state.countries);
+  const sortCountries = countries.sort(function compareName(a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+  let [newCountries, setNewCountries] = useState(sortCountries);
   let [values, setValues] = useState({
     name: "",
     difficulty: 1,
     duration: 0,
     season: "",
   });
+  let [countryAdd, setCountryAdd] = useState([]);
+  let [countrySelect, setCountrySelect] = useState(newCountries[0].name);
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -22,8 +34,37 @@ export default function CreateActivity() {
       ? setValues({ ...values, difficulty: parseInt(e.target.value) })
       : e.target.name === "duration"
       ? setValues({ ...values, duration: e.target.value })
-      : e.target.name === "season" &&
-        setValues({ ...values, season: e.target.value });
+      : e.target.name === "season"
+      ? setValues({ ...values, season: e.target.value })
+      : e.target.name === "country" && setCountrySelect(e.target.value);
+  };
+  const onClick = () => {
+    console.log("Agregado");
+    setCountryAdd([...countryAdd, countrySelect]);
+    let newArray = newCountries.filter(
+      (countryDelete) => countrySelect !== countryDelete.name
+    );
+    console.log(newArray);
+    setNewCountries(newArray);
+    setCountrySelect(newArray[0].name);
+  };
+  const handleEliminate = (country) => {
+    let newArray = countryAdd.filter(
+      (countryDelete) => country !== countryDelete
+    );
+    setCountryAdd(newArray);
+    let array = newCountries;
+    array.push({ name: country });
+    let sortArray = array.sort(function compareName(a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    });
+    setNewCountries(sortArray);
   };
   return (
     <>
@@ -99,15 +140,26 @@ export default function CreateActivity() {
         <label>
           <div className={style.selector}>Country:</div>
           <select name="country" onChange={handleOnChange}>
-            {countries.map((country) => (
+            {newCountries.map((country) => (
               <option key={country.id} value={country.name}>
                 {country.name}
               </option>
             ))}
           </select>
+          <button onClick={onClick}>Add</button>
         </label>
         <br />
         <br />
+        {countryAdd.length !== 0 &&
+          countryAdd.map((country) => (
+            <div key={country}>
+              <div className={style.add}>
+                <div>{country}</div>
+                <button onClick={() => handleEliminate(country)}>X</button>
+              </div>
+              <br />
+            </div>
+          ))}
         <input type="submit" value="Submit" />
       </form>
     </>
